3304a0cc31b6f47317132169ef103a322bdafd49
Hannes Krumbiegel <Vuizur@users.noreply.github.com>
2023-06-21 22:03:15 +0200

    GH actions test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    "output": "export"
+    images: {
+        loader: 'akamai',
+        path: '',
+      },
+      assetPrefix: './',
 }
 
 module.exports = nextConfig
