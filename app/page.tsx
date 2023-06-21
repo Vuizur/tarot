cf7b22fbc1849582e1d5df5fb954aadaa1d98fca
Hannes Krumbiegel <Vuizur@users.noreply.github.com>
2023-06-21 21:00:49 +0200

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,59 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
 
-export default function Home() {
+import { useState } from "react";
+import { Button, Card, CardMedia, Container, Typography, Box } from "@mui/material";
+import { Shuffle } from "@mui/icons-material";
+
+// The number of tarot cards you have
+const NUM_CARDS = 78;
+
+// A function that returns a random integer between 1 and n (inclusive)
+const randomInt = (n: number) => Math.floor(Math.random() * n) + 1;
+
+// The main component of the app
+const App = () => {
+  // A state variable that stores the current card number
+  const [card, setCard] = useState(randomInt(NUM_CARDS));
+
+  // A function that shuffles the card
+  const shuffleCard = () => {
+    setCard(randomInt(NUM_CARDS));
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container maxWidth="sm">
+      <Box sx={
+        {
+          height: "100vh",
+          flexDirection: "column",
+          display: "flex",
+          p: 2,
+        }
+      }>
+        <Typography variant="h4" align="center" gutterBottom>
+          Draw a Tarot Card
+        </Typography>
+        <Card>
+          <CardMedia
+            component="img"
+            image={`cards/card-${card}.jpg`}
+            alt={`Tarot card ${card}`}
+            sx={{ height: "100%",
+            objectFit: "contain" }}
+          />
+        </Card>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<Shuffle />}
+          onClick={shuffleCard}
+          fullWidth
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          Shuffle
+        </Button>
+      </Box>
+    </Container >
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default App;
