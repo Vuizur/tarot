46b1d79ce5b989455003e2e6f8e8533ac897f051
Hannes Krumbiegel <Vuizur@users.noreply.github.com>
2023-06-21 22:38:30 +0200

    New try

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+}
+
+module.exports = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
     images: {
         loader: 'akamai',
         path: '',
-      },
-      assetPrefix: './',
+    }
 }
-
-module.exports = nextConfig
-
