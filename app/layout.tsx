b22794b81d9463869ed9feb0ac31049a08dedfa1
Vuizur <Vuizur@users.noreply.github.com>
2023-11-14 22:37:08 +0100

    Remove / in front of manifest.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
     template: APP_TITLE_TEMPLATE,
   },
   description: APP_DESCRIPTION,
-  manifest: "/manifest.json",
+  manifest: "manifest.json",
   themeColor: "#FFFFFF",
   appleWebApp: {
     capable: true,
